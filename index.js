ee227d371dd2f572dfe39e4582556fd3096eb19d
Keshav Joshi <43142500+ItIsNotKeshav@users.noreply.github.com>
2024-02-11 00:56:05 +0530

    Added functionality

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-/*
-const cbtn = document.getElementById('c');
-const entbtn = document.getElementById('ent-btn');
-const delbtn = document.getElementById('del');
-cbtn.addEventListener("mouseover", (event) => {
-    const x = event.pageX - cbtn.offsetLeft;
-    const y = event.pageY - cbtn.offsetTop;
-  
-    cbtn.style.setProperty("--xPos", x + "px");
-    cbtn.style.setProperty("--yPos", y + "px");
-  });
-
-entbtn.addEventListener("mouseover", (event) => {
-    const x = event.pageX - entbtn.offsetLeft;
-    const y = event.pageY - entbtn.offsetTop;
-  
-    entbtn.style.setProperty("--xPos", x + "px");
-    entbtn.style.setProperty("--yPos", y + "px");
-  });
-
-delbtn.addEventListener("mouseover", (event) => {
-    const x = event.pageX - delbtn.offsetLeft;
-    const y = event.pageY - delbtn.offsetTop;
-  
-    delbtn.style.setProperty("--xPos", x + "px");
-    delbtn.style.setProperty("--yPos", y + "px");
-  });
-*/
-
 const buttons = document.querySelectorAll('.btn, .ent-btn');
 
 buttons.forEach((button) => {
@@ -38,3 +9,64 @@ buttons.forEach((button) => {
         button.style.setProperty("--yPos", y + "px");
     });
 });
+
+let displayValue = '';
+
+function addNumber(num) {
+  if (displayValue.length <= 16){
+    displayValue += num;
+    maxVal();
+  }
+    updateDisplay();
+}
+
+function addOperator(operator) {
+    displayValue += operator;
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '';
+    maxVal();
+    updateDisplay();
+}
+
+function deleteLast() {
+    displayValue = displayValue.slice(0, -1);
+    maxVal();
+    updateDisplay();
+}
+
+function calculate() {
+    try {
+        displayValue = eval(displayValue);
+        updateDisplay();
+    } catch (error) {
+        displayValue = 'Error';
+        updateDisplay();
+    }
+}
+
+function maxVal() {
+  var display = document.querySelector('.display');
+  if (displayValue.length > 14) {
+    display.style.fontSize = "16px";
+  } else if (displayValue.length > 12) {
+    display.style.fontSize = "20px";
+  }
+  else if (displayValue.length > 10) {
+    display.style.fontSize = "24px";
+  }
+  else if (displayValue.length > 8) {
+    display.style.fontSize = "28px";
+  }
+  else {
+    display.style.fontSize = "36px"; // or whatever your default size is
+  }
+  updateDisplay();
+}
+
+function updateDisplay() {
+    document.querySelector('.display').innerText = displayValue;
+}
+
