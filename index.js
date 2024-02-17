0f8982f42e7f7d07d4e3df3e5b84b7e416015467
Keshav Joshi <43142500+ItIsNotKeshav@users.noreply.github.com>
2024-02-17 23:25:51 +0530

    Themeing completed.

--- index.js
+++ index.js
@@ -80,19 +80,17 @@ function updateDisplay() {
   document.querySelector(".display").innerText = displayValue;
 }
 
-
 function bracketcheck() {
-  displayValue = displayValue.replace(/\(/g, '*(');
-  displayValue = displayValue.replace(/\)/g, ')*');
+  displayValue = displayValue.replace(/\(/g, "*(");
+  displayValue = displayValue.replace(/\)/g, ")*");
 }
 
 function operatorend() {
-  let operators = ["+", "-", "*", "/", "(" ];
+  let operators = ["+", "-", "*", "/", "("];
   if (operators.includes(displayValue[displayValue.length - 1])) {
     displayValue = displayValue.slice(0, -1);
   }
 }
-
 
 function calculate() {
   try {
@@ -100,12 +98,9 @@ function calculate() {
     if (!isNaN(displayValue)) {
       return displayValue;
     }
-    console.log(displayValue);
-    displayValue = displayValue.replace(/\^/g, '**'); // Replace ^ with ** to make power work
+    displayValue = displayValue.replace(/\^/g, "**"); // Replace ^ with ** to make power work
     bracketcheck();
-    console.log(displayValue);
     operatorend();
-    console.log(displayValue);
     displayValue = eval(displayValue);
     maxVal();
     updateDisplay();
@@ -115,121 +110,156 @@ function calculate() {
   }
 }
 
-
 let themeColors = {
-  purple: {
-    backgroundColorLight: '#F5F2F9',
-    backgroundColorDark: '#080210',
-    textLight: '#080210',
-    textDark: '#F5F2F9',
-    darkest: '#43147c',
-    medDark: '#873fde',
-    medLight: '#a772e9',
-    light: '#BD96ED',
-    ent: '#7C2FDA',
-    frame: '#D3B9F3',
+  purpleLight: {
+    backColor: "#F5F2F9",
+    textColor: "#080210",
+    darkest: "#43147c",
+    medDark: "#873fde",
+    medLight: "#a772e9",
+    light: "#BD96ED",
+    ent: "#7C2FDA",
+    frame: "#D3B9F3",
   },
-  red: {
-    backgroundColorLight: '#FDF5F5',
-    backgroundColorDark: '#0D0102',
-    textLight: '#FDF5F5',
-    textDark: '#0D0102',
-    darkest: '#A91521',
-    medDark: '#E51022',
-    medLight: '#F2404F',
-    light: '#F67984',
-    ent: '#E51022',
-    frame: '#EE929A',
+  purpleDark: {
+    backColor: "#080210",
+    textColor: "#F5F2F9",
+    darkest: "#43147c",
+    medDark: "#873fde",
+    medLight: "#a772e9",
+    light: "#BD96ED",
+    ent: "#7C2FDA",
+    frame: "#D3B9F3",
+  },
+  redLight: {
+    backColor: "#FDF5F5",
+    textColor: "#0D0102",
+    darkest: "#A91521",
+    medDark: "#E51022",
+    medLight: "#F2404F",
+    light: "#F67984",
+    ent: "#E51022",
+    frame: "#EE929A",
+  },
+  redDark: {
+    backColor: "#0D0102",
+    textColor: "#FDF5F5",
+    darkest: "#A91521",
+    medDark: "#E51022",
+    medLight: "#F2404F",
+    light: "#F67984",
+    ent: "#E51022",
+    frame: "#EE929A",
   },
   green: {
-    backgroundColorLight: '#F3FBED',
-    backgroundColorDark: '#071001',
-    textLight: '#071001',
-    textDark: '#F3FBED',
-    darkest: '#2A6600',
-    medDark: '#4E9819',
-    medLight: '#5BC212',
-    light: '#8FF546',
-    ent: '#0E9E0E',
-    frame: '#C7E8B0',
+    backColor: "#F3FBED",
+    textColor: "#071001",
+    darkest: "#2A6600",
+    medDark: "#4E9819",
+    medLight: "#5BC212",
+    light: "#8FF546",
+    ent: "#0E9E0E",
+    frame: "#C7E8B0",
   },
-  blue: {
-    backgroundColorLight: '#E7EFF8',
-    backgroundColorDark: '#040C16',
-    textLight: '#040C16',
-    textDark: '#E7EFF8',
-    darkest: '#13273E',
-    medDark: '#22456D',
-    medLight: '#366CAB',
-    light: '#6495CE',
-    ent: '#3B84D9',
-    frame: '#BCD1EA',
+  greenDark: {
+    backColor: "#071001",
+    textColor: "#F3FBED",
+    darkest: "#2A6600",
+    medDark: "#4E9819",
+    medLight: "#5BC212",
+    light: "#8FF546",
+    ent: "#0E9E0E",
+    frame: "#C7E8B0",
   },
-  yellow: {
-    backgroundColorLight: '#FBF7EC',
-    backgroundColorDark: '#0A0701',
-    textLight: '#0A0701',
-    textDark: '#FBF7EC',
-    darkest: '#C28F18',
-    medDark: '#E8AB1E',
-    medLight: '#F0C45F',
-    light: '#F5D895',
-    ent: '#E5AB24',
-    frame: '#F0DFB8',
+  blueLight: {
+    backColor: "#E7EFF8",
+    textColor: "#040C16",
+    darkest: "#13273E",
+    medDark: "#22456D",
+    medLight: "#366CAB",
+    light: "#6495CE",
+    ent: "#3B84D9",
+    frame: "#BCD1EA",
   },
-}
+  blueDark: {
+    backColor: "#040C16",
+    textColor: "#E7EFF8",
+    darkest: "#13273E",
+    medDark: "#22456D",
+    medLight: "#366CAB",
+    light: "#6495CE",
+    ent: "#3B84D9",
+    frame: "#BCD1EA",
+  },
+  yellowLight: {
+    backColor: "#FBF7EC",
+    textColor: "#0A0701",
+    darkest: "#C28F18",
+    medDark: "#E8AB1E",
+    medLight: "#F0C45F",
+    light: "#F5D895",
+    ent: "#E5AB24",
+    frame: "#F0DFB8",
+  },
+  yellowDark: {
+    backColor: "#191202",
+    textColor: "#FBF7EC",
+    darkest: "#C28F18",
+    medDark: "#E8AB1E",
+    medLight: "#F0C45F",
+    light: "#F5D895",
+    ent: "#E5AB24",
+    frame: "#F0DFB8",
+  },
+};
 
 let themeColorsArray = Object.values(themeColors);
 
+let currentThemeIndex = 2;
 
-let currentThemeIndex = 0;
+window.onload = changeTheme;
 
 function changeTheme() {
   currentThemeIndex = (currentThemeIndex + 1) % themeColorsArray.length;
   const newThemeColor = themeColorsArray[currentThemeIndex];
 
-  // Apply the new theme colors to the body and button elements
-  document.body.style.backgroundColor = newThemeColor.backgroundColorDark;
-  document.getElementById('themeBtn').style.backgroundColor = newThemeColor.backgroundColorLight;
-  
+  document.body.style.backgroundColor = newThemeColor.backColor;
 
-  document.querySelectorAll('.dark').forEach((button) => {
+  document.getElementById("themeBtn").style.backgroundColor =
+    newThemeColor.darkest;
+
+  document.querySelector(".display").style.borderColor = newThemeColor.frame;
+
+  document.querySelector(".display").style.color = newThemeColor.textColor;
+
+  document.querySelectorAll(".dark").forEach((button) => {
     button.style.backgroundColor = newThemeColor.darkest;
   });
-  document.querySelectorAll('.med-dark').forEach((button) => {
+
+  document.querySelectorAll(".med-dark").forEach((button) => {
     button.style.backgroundColor = newThemeColor.medDark;
   });
 
-  document.querySelectorAll('.med-light').forEach((button) => {
+  document.querySelectorAll(".med-light").forEach((button) => {
     button.style.backgroundColor = newThemeColor.medLight;
   });
 
-  document.querySelectorAll('.light').forEach((button) => {
+  document.querySelectorAll(".light").forEach((button) => {
     button.style.backgroundColor = newThemeColor.light;
   });
 
-  document.querySelectorAll('.ent-btn').forEach((button) => {
+  document.querySelectorAll(".ent-btn").forEach((button) => {
     button.style.backgroundColor = newThemeColor.ent;
   });
 
-  
-
-
-  document.querySelector('.ent-btn').style.backgroundColor = newThemeColor.ent;
+  document.querySelector(".ent-btn").style.backgroundColor = newThemeColor.ent;
 }
 
-let isDarkMode = false;
-
-function toggleThemeMode() {
-  isDarkMode = !isDarkMode;
-  const themeMode = isDarkMode ? 'dark-mode' : 'light-mode';
-  document.body.classList.toggle(themeMode);
-}
-
-// Adding animation to the button
-const themeBtn = document.getElementById('themeBtn');
-themeBtn.addEventListener('mouseover', (event) => {
-  themeBtn.style.setProperty('--xPos', event.pageX - themeBtn.offsetLeft + 'px');
-  themeBtn.style.setProperty('--yPos', event.pageY - themeBtn.offsetTop + 'px');
+const themeBtn = document.getElementById("themeBtn");
+themeBtn.addEventListener("mouseover", (event) => {
+  themeBtn.style.setProperty(
+    "--xPos",
+    event.pageX - themeBtn.offsetLeft + "px"
+  );
+  themeBtn.style.setProperty("--yPos", event.pageY - themeBtn.offsetTop + "px");
 });
-
